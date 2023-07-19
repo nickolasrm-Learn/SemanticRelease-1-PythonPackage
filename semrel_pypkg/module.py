94cb2838bf358575b539adeb9d2e8287bdd0f79b
nickolasrm <nickolasrochamachado@gmail.com>
2023-07-19 19:49:57 +0000

    refactor: :fire: remove add function
    BREAKING: removed add function
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- semrel_pypkg/module.py
+++ semrel_pypkg/module.py
@@ -1,9 +1,3 @@
-
-def add(a: int, b: int) -> int:
-    """Add two numbers together."""
-    return a + b
-
-
 def sub(a: int, b: int) -> int:
     """Subtract two numbers."""
     return a - b
--- tests/test_add.py
+++
@@ -1,5 +0,0 @@
-import semrel_pypkg.module
-
-
-def test_add():
-    assert semrel_pypkg.module.add(3,2) == 5
